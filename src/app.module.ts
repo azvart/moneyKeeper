fb5e7ed5e42d0de018c2a0889b4fbb7b06f18aaf
vladgasanov <vlad.gasanov@zing.coach>
2025-02-23 14:24:59 +0100

    initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AccountModule } from './account/account.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        uri: (await config.get('MONGO_URI')) || process.env.MONGO_URI,
-      }),
-    }),
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
     AccountModule,
   ],
 })
